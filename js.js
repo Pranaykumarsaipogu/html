dec75332511990ad0125c207cf73c9fd3a5672c1
Pranaykumarsaipogu <108129698+Pranaykumarsaipogu@users.noreply.github.com>
2022-06-24 17:01:40 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,45 @@
+var marks=75;
+if(marks>35)
+{
+  console.log("pass");
+}
+else
+{
+  console.log("fail");
+}
+  
+var n=10;
+if(n>0)
+  {
+    console.log("positive");
+  }
+else if(n<0)
+  {
+    console.log("negative");
+  }
+ else
+   {
+     console.log("zero");
+   }
+
+var m=100;
+if(m>100)
+  {
+    console.log("undefined");
+  }
+else if(m>80)
+  {
+    console.log("grade A");
+  }
+else if(m>60)
+  {
+    console.log("grade B");
+  }
+else if(m>35)
+  {
+    console.log("grade C");
+  }
+else
+    {
+     console.log("fail");
+     }
